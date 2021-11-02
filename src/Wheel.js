d31c485fb74b495df4e0a91d948815d72db2ff79
Chirag <chiragvalecha97@gmail.com>
2021-11-02 22:19:29 +0530

    add event listeners to the wheel

--- src/Wheel.js
+++ src/Wheel.js
@@ -1,10 +1,57 @@
 import React, { Component } from "react";
+import ZingTouch from "zingtouch";
 import "./css/wheel.css";
+
 class Wheel extends Component {
+  constructor() {
+    super();
+    this.state = {
+      index: 1,
+    };
+  }
+
+  //Bind components with zingtouch to control wheel rotation by 15deg
+  componentDidMount() {
+    var wheel = document.getElementById("wheel");
+    var activeRegion = ZingTouch.Region(wheel);
+
+    const menu = document.getElementById("menu");
+    const forward = document.getElementById("forward");
+    const reverse = document.getElementById("reverse");
+    const playPauseToggle = document.getElementById("play-pause");
+    activeRegion.bind(wheel, "rotate", (e) => {
+      if (e.detail.distanceFromOrigin !== 0) {
+        var temp = Math.floor(Math.abs(e.detail.distanceFromOrigin) / 15);
+        this.setState({
+          index: (this.state.index + temp) % 4,
+        });
+      }
+      console.log(this.state.index);
+    });
+
+    activeRegion.bind(menu, "tap", (e) => {
+      console.log("Menu button clicked");
+    });
+    activeRegion.bind(playPauseToggle, "tap", (e) => {
+      console.log("Play-pause button clicked");
+    });
+
+    const longTapGesture = new ZingTouch.Tap({
+      maxDelay: 10000,
+      numInputs: 1,
+      tolerance: 1,
+    });
+    activeRegion.bind(forward, longTapGesture, (e) => {
+      console.log("forward button clicked");
+    });
+    activeRegion.bind(reverse, longTapGesture, (e) => {
+      console.log("backward button pressed");
+    });
+  }
   render() {
     return (
       <div className="wheel-container">
-        <div className="wheel-ui">
+        <div className="wheel-ui" id="wheel">
           <div className="control" id="menu">
             Menu
           </div>
